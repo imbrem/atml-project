948929b9cbf5bf90b9c56806c4b4bc077f15222a
kamilest <stankeviciute.kamile@gmail.com>
2021-03-29 11:52:52 +0100

    Simplify RNN parameter code.

--- rnn_parameters.py
+++ rnn_parameters.py
@@ -6,45 +6,29 @@ def get_baseline_parameters(model, task_id):
     root_dir = 'babi_data'
     n_train_to_try = [0]
     learning_rate = 1e-3
-    batch_size = 10
+    batch_size = 100
     max_iters = 1000
-    print_every = 10
-    save_every = 100
+    print_every = 100
+    save_every = 2000
     n_targets = 1
 
-    if task_id is 'babi4':
+    if task_id == 4:
         n_train_to_try = [50, 100, 250, 500, 950]
         batch_size = 20
-        print_every = 100
-        save_every = 2000
-        task_id = 4
-    elif task_id is 'babi5':
+    elif task_id == 5:
         max_iters = 20000 if model is 'rnn' else 5000
-        batch_size = 100
-        print_every = 100
-        save_every = 2000
-        task_id = 5
-    elif task_id is 'babi16':
+    elif task_id == 16:
         max_iters = 20000 if model is 'rnn' else 5000
         learning_rate = 0.0005
-        batch_size = 100
-        print_every = 100
-        save_every = 2000
-        task_id = 16
-    elif task_id is 'babi18':
+    elif task_id == 18:
         max_iters = 500
         learning_rate = 0.0005
-        batch_size = 100
         print_every = 10
         save_every = 1000
-        task_id = 18
-    elif task_id is 'babi19':
+    elif task_id == 19:
         max_iters = 10000 if model is 'rnn' else 5000
         batch_size = 20
-        print_every = 100
-        save_every = 2000
         n_targets = 2
-        task_id = 19
 
     parameters = {'model': model,
                   'embedding_size': embedding_size,
