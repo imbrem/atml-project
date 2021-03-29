d0791b4e4fafbc382995a491eb74070407225990
kamilest <stankeviciute.kamile@gmail.com>
2021-03-29 12:04:53 +0100

    Separate default parameters/avoid code duplication.

--- rnn_parameters.py
+++ rnn_parameters.py
@@ -1,46 +1,42 @@
-def get_baseline_parameters(model, task_id):
+def get_default_parameters():
+    parameters = {'model': 'rnn',
+                  'task_id': 4,
+                  'embedding_size': 50,
+                  'hidden_size': 50,
+                  'n_targets': 1,
+                  'learning_rate': 1e-3,
+                  'batch_size': 100,
+                  'max_iters': 1000,
+                  'root_dir': 'babi_data',
+                  'n_train_to_try': [0],
+                  'print_every': 100,
+                  'save_every': 2000}
+
+    return parameters
+
+
+def get_parameters_for_task(model, task_id):
     """Returns the parameters for a given task and model."""
-    # Default arguments
-    embedding_size = 50
-    hidden_size = 50
-    root_dir = 'babi_data'
-    n_train_to_try = [0]
-    learning_rate = 1e-3
-    batch_size = 100
-    max_iters = 1000
-    print_every = 100
-    save_every = 2000
-    n_targets = 1
+    parameters = get_default_parameters()
+    parameters['model'] = model
+    parameters['task_id'] = task_id
 
     if task_id == 4:
-        n_train_to_try = [50, 100, 250, 500, 950]
-        batch_size = 20
+        parameters['n_train_to_try'] = [50, 100, 250, 500, 950]
+        parameters['batch_size'] = 20
     elif task_id == 5:
-        max_iters = 20000 if model is 'rnn' else 5000
+        parameters['max_iters'] = 20000 if model is 'rnn' else 5000
     elif task_id == 16:
-        max_iters = 20000 if model is 'rnn' else 5000
-        learning_rate = 0.0005
+        parameters['max_iters'] = 20000 if model is 'rnn' else 5000
+        parameters['learning_rate'] = 0.0005
     elif task_id == 18:
-        max_iters = 500
-        learning_rate = 0.0005
-        print_every = 10
-        save_every = 1000
+        parameters['max_iters'] = 500
+        parameters['learning_rate'] = 0.0005
+        parameters['print_every'] = 10
+        parameters['save_every'] = 1000
     elif task_id == 19:
-        max_iters = 10000 if model is 'rnn' else 5000
-        batch_size = 20
-        n_targets = 2
-
-    parameters = {'model': model,
-                  'embedding_size': embedding_size,
-                  'hidden_size': hidden_size,
-                  'n_targets': n_targets,
-                  'learning_rate': learning_rate,
-                  'batch_size': batch_size,
-                  'max_iters': max_iters,
-                  'root_dir': root_dir,
-                  'task_id': task_id,
-                  'n_train_to_try': n_train_to_try,
-                  'print_every': print_every,
-                  'save_every': save_every}
+        parameters['max_iters'] = 10000 if model is 'rnn' else 5000
+        parameters['batch_size'] = 20
+        parameters['n_targets'] = 2
 
     return parameters
