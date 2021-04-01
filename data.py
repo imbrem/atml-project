c458a7e64f395ae21e66052d7434908946fd5d25
kamilest <stankeviciute.kamile@gmail.com>
2021-04-01 19:21:32 +0100

    BabiSequentialGraphDataset

--- data.py
+++ data.py
@@ -3,21 +3,15 @@ Data preprocessing module.
 
 Converts the bAbI dataset files into tensors ready for training.
 
-This is not original code, but is adapted from the bAbI dataset processing code from
-https://github.com/chingyaoc/ggnn.pytorch/blob/master/utils/data/dataset.py and
+The code is adapted from the bAbI dataset processing code at:
+https://github.com/chingyaoc/ggnn.pytorch/blob/master/utils/data/dataset.py
 https://github.com/chingyaoc/ggnn.pytorch/blob/master/utils/data/dataloader.py
-
-RNN processing part adapts the original code at
-https://github.com/yujiali/ggnn/blob/master/babi/babi_data.lua
 """
-
 
 import numpy as np
 import torch
-from torch.utils.data import DataLoader
 from torch_geometric.data import Data
-
-# GRAPH DATA
+import baselines_data
 
 
 def load_graphs_from_file(file_name):
@@ -81,7 +75,8 @@ def split_set(data_list):
     idx = range(n_examples)
     train = idx[:50]
     val = idx[-50:]
-    return np.array(data_list, dtype=object)[train], np.array(data_list, dtype=object)[val]
+    return np.array(data_list, dtype=object)[train], \
+           np.array(data_list, dtype=object)[val]
 
 
 def data_convert(data_list, n_annotation_dim):
@@ -97,7 +92,7 @@ def data_convert(data_list, n_annotation_dim):
             task_type = target[0]
             task_output = target[-1]
             annotation = np.zeros([n_nodes, n_annotation_dim])
-            annotation[target[1]-1][0] = 1
+            annotation[target[1] - 1][0] = 1
             task_data_list[task_type -
                            1].append([edge_list, annotation, task_output])
     return task_data_list
@@ -109,8 +104,8 @@ def create_adjacency_matrix(edges, n_nodes, n_edge_types):
         src_idx = edge[0]
         e_type = edge[1]
         tgt_idx = edge[2]
-        a[tgt_idx-1][(e_type - 1) * n_nodes + src_idx - 1] = 1
-        a[src_idx-1][(e_type - 1 + n_edge_types) * n_nodes + tgt_idx - 1] = 1
+        a[tgt_idx - 1][(e_type - 1) * n_nodes + src_idx - 1] = 1
+        a[src_idx - 1][(e_type - 1 + n_edge_types) * n_nodes + tgt_idx - 1] = 1
     return a
 
 
@@ -119,17 +114,18 @@ def create_pg_graph(datapoint, n_edge_types):
     edges, annotations, target = datapoint
     x = torch.FloatTensor(annotations)
     directed_edge_index = torch.LongTensor(
-        [[edge[0]-1, edge[2]-1] for edge in edges])
+        [[edge[0] - 1, edge[2] - 1] for edge in edges])
     reverse_edge_index = torch.index_select(
         directed_edge_index, 1, torch.LongTensor([1, 0]))
     edge_index = torch.cat([directed_edge_index, reverse_edge_index], dim=0).T
     # print("Edge index", edge_index)
 
     edge_type_indices = torch.LongTensor(
-        [[i, edge[1]-1] for i, edge in enumerate(edges)])
+        [[i, edge[1] - 1] for i, edge in enumerate(edges)])
     # print("Edge type", edge_type_indices)
     reverse_edge_type_indices = torch.LongTensor(
-        [[i+len(edges), edge[1]-1+n_edge_types] for i, edge in enumerate(edges)])
+        [[i + len(edges), edge[1] - 1 + n_edge_types] for i, edge in
+         enumerate(edges)])
     full_edge_type_indices = torch.cat(
         [edge_type_indices, reverse_edge_type_indices], dim=0)
     # print("Full edge", full_edge_type_indices)
@@ -138,8 +134,17 @@ def create_pg_graph(datapoint, n_edge_types):
     for edge_type in full_edge_type_indices.numpy().tolist():
         edge_attr[tuple(edge_type)] = 1
 
-    y = torch.FloatTensor([datapoint[2]-1]).view(1, 1)
+    y = torch.FloatTensor([datapoint[2] - 1]).view(1, 1)
     return Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
+
+
+def get_sequential_graph(sequence, target):
+    # sequence: [seq_len, max_node_id]
+    # target: [n_targets]
+    edge_index = torch.tensor(
+        (range(sequence.size(0)), range(1, sequence.size(0) + 1)),
+        dtype=torch.long)
+    return Data(x=sequence, edge_index=edge_index, y=target)
 
 
 class bAbIDataset:
@@ -167,6 +172,57 @@ class bAbIDataset:
 
     def __len__(self):
         return len(self.data)
+
+
+class BabiSequentialGraphDataset:
+    """ Loads the sequential RNN data as a linear graph for GGS-NN training.
+
+    Obtains the file (training, validation, or test) for the corresponding
+    fold from the root directory containing the entire dataset (will likely
+    be babi_data/ in this case).
+    """
+
+    def __init__(self, root_dir, fold_id, task_id, n_targets=1,
+                 split='train', n_train=0):
+        """
+        Args:
+            root_dir (str): Path to the dataset root directory ('babi_data/')
+            fold_id (int): The number of the fold.
+            task_id (int): The ID of the bAbI task.
+            n_targets (int): The length of output sequences.
+            split (str): One of 'train', 'validation', 'test'
+            n_train (int): How many training instances to use.
+        """
+
+        filename = baselines_data.get_data_filename(root_dir, fold_id,
+                                                    task_id, split)
+        max_token_id = baselines_data.get_max_token_id(root_dir, task_id,
+                                                       n_targets)
+
+        if split is 'validation':
+            filename = filename.parent / (filename.name + '.val')
+
+        if split is not 'train':
+            n_train = 0
+
+        sequences, targets = \
+            baselines_data.get_sequence_and_target_lists_from_file(
+                filename,
+                n_targets,
+                n_train)
+
+        one_hot_sequences = baselines_data.one_hot_token_list(sequences,
+                                                              max_token_id)
+
+        self.graphs = []
+        for sequence, target in zip(one_hot_sequences, targets):
+            self.graphs.append(get_sequential_graph(sequence, target))
+
+    def __len__(self):
+        return len(self.graphs)
+
+    def __getitem__(self, idx):
+        return self.graphs[idx], self.graphs[idx]
 
 
 if __name__ == "__main__":
