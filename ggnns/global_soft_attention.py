bd583deedddb34d24d0bb34fcd03443283785719
kamilest <stankeviciute.kamile@gmail.com>
2021-04-03 18:29:19 +0100

    Add comments, formatting.

--- ggnns/global_soft_attention.py
+++ ggnns/global_soft_attention.py
@@ -5,11 +5,21 @@ from typing import Union, List, Optional
 
 
 def make_graph_attention(
-    gate_nn: Module,
-    nn: Optional[Module] = None,
-    graph_attention_impl='torch_geometric',
-    **kwargs
+        gate_nn: Module,
+        nn: Optional[Module] = None,
+        graph_attention_impl='torch_geometric',
+        **kwargs
 ):
+    """Global soft attention layer.
+
+    Args:
+        gate_nn (torch.nn.Module): A neural network that computes attention
+            scores by mapping node features `x` of shape `[-1, in_channels]`
+            to shape `[-1, 1]`.
+        nn (torch.nn.Module, optional): A neural network that maps node
+            features `x` of shape `[-1, in_channels]` to shape :`[-1,
+            out_channels]` before combining them with the attention scores.
+    """
     if graph_attention_impl == 'torch_geometric':
         return torch_geometric.nn.GlobalAttention(gate_nn=gate_nn, nn=nn)
     elif graph_attention_impl == 'team2':
