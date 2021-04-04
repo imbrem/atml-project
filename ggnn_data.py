f4c594560765b0bef34bd91995fae0ac3f8f30db
kamilest <stankeviciute.kamile@gmail.com>
2021-04-04 13:00:07 +0100

    Fix ggnn_data.py __getitem__ to return one graph copy.

--- ggnn_data.py
+++ ggnn_data.py
@@ -291,7 +291,7 @@ class BabiSequentialGraphDataset:
         return len(self.graphs)
 
     def __getitem__(self, idx):
-        return self.graphs[idx], self.graphs[idx]
+        return self.graphs[idx]
 
 
 def get_data_loaders(params, fold_id, n_train, dataset='sequential_graph'):
